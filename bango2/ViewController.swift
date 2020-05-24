5dc29956a5420b501950d3951d18f009ae8d72a1
sen1000-byte <nishiwaki.chihiro@gmail.com>
2020-05-24 10:41:05 +0900

    Initial Commit

---
+++ bango2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bango2
+//
+//  Created by Chihiro Nishiwaki on 2020/05/24.
+//  Copyright © 2020 Nishiwaki sen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
